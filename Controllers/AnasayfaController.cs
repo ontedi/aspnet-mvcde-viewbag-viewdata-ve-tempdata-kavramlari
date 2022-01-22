f01c2645cd7041c16914415c919628ae7057cede
ontedi <universityofcu@gmail.com>
2022-01-22 16:05:20 +0300

    first commit

---
+++ Controllers/AnasayfaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace aspnet_mvcde_viewbag_viewdata_ve_tempdata_kavramlari.Controllers
+{
+    public class AnasayfaController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["AnasayfaViewDataVerisi"] = "ViewData içerisindeki veriler";
+            ViewBag.AnasayfaViewBagVerisi = "ViewBag içerisindeki veriler";
+            TempData["AnasayfaTempDataVerisi"] = "TempData içerisindeki veriler";
+            return View();
+        }
+    }
+}
---
+++ Controllers/IletisimController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_mvcde_viewbag_viewdata_ve_tempdata_kavramlari.Controllers
+{
+    public class IletisimController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
